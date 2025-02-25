530c3f51a160c14a55fe2cfd08e45c8386bbc5c6
Shubhangipawar13 <2023bit048@sggs.ac.in>
2025-02-25 16:08:00 +0530

    Add files via upload

--- Practical9.java
+++ Practical9.java
@@ -1,7 +1,7 @@
 class Practical9 {
 
 // Comparing time of Iterative and Recursive method of factorial calculation 
- int[] random_num(int size){
+public static int[] random_num(int size){
  	int[] random_number_array = new int[size];
  	for(int i=0; i<size; i++){
  		random_number_array[i] = (int)((System.nanoTime()%100)+1);
@@ -25,32 +25,35 @@ public static long factorial2(int num){
 	return num*factorial2(num-1);
 }    
 
-public static long avg_timefor_fact1(int[] random_number_array) {
-        long total_time = 0; 
+public static void avg_timefor_fact(int[] random_number_array) {
+        long total_time1 = 0; 
+        long total_time2 = 0; 
 
         for (int i = 0; i < random_number_array.length; i++) {
             long start_time = System.nanoTime();  
             long fact = factorial1(random_number_array[i]);  
             long end_time = System.nanoTime();  
-            total_time += (end_time - start_time); 
+            total_time1 += (end_time - start_time); 
         }
-
-        long avg_time = total_time / random_number_array.length; 
-        return avg_time;
-    }
-    
-public static long avg_timefor_fact2(int[] random_number_array) {
-        long total_time = 0; 
-
+        
         for (int i = 0; i < random_number_array.length; i++) {
             long start_time = System.nanoTime();  
             long fact = factorial2(random_number_array[i]);  
             long end_time = System.nanoTime();  
-            total_time += (end_time - start_time); 
+            total_time2 += (end_time - start_time); 
         }
 
-        long avg_time = total_time / random_number_array.length; 
-        return avg_time;
+        long avg_time1 = total_time1 / random_number_array.length; 
+        long avg_time2 = total_time2 / random_number_array.length; 
+        System.out.println("Iteration time is: "+ avg_time1);
+        System.out.println("Recursive time is: "+ avg_time2);
+        if(avg_time1<avg_time2){
+          System.out.println("Iteration is more efficient");
+        }
+        else{
+          System.out.println("Recursion is more efficient");
+        }
+        
     }  
 
 
@@ -85,7 +88,7 @@ public static void time_takenby_Stringto_concatenate(int size) {
     String str_concat = str + str;
     long end_time1 = System.nanoTime();
     long String_time_diff = end_time1 - start_time1;
-    System.out.println(String_time_diff);
+    System.out.println("Time taken by String to concatenate: "+ String_time_diff);
     
     
     StringBuffer sBuff = new StringBuffer(str);
@@ -93,14 +96,14 @@ public static void time_takenby_Stringto_concatenate(int size) {
     sBuff.append(str);
     long end_time2 = System.nanoTime();
     long StringBuffer_time_diff = end_time2 - start_time2;
-    System.out.println(StringBuffer_time_diff);
+    System.out.println("Time taken by StringBuffer to append: "+ StringBuffer_time_diff);
     
     StringBuilder sBuild = new StringBuilder(str);
     long start_time3 = System.nanoTime();
     sBuild.append(str);
     long end_time3 = System.nanoTime();
     long StringBuilder_time_diff = end_time3 - start_time3;
-    System.out.println(StringBuilder_time_diff);
+    System.out.println("Time taken by StringBuilder to append: "+ StringBuilder_time_diff);
     
     // Determining the most efficient method
     if (String_time_diff < StringBuffer_time_diff && String_time_diff < StringBuilder_time_diff) {
@@ -112,6 +115,14 @@ public static void time_takenby_Stringto_concatenate(int size) {
     else {
         System.out.println("StringBuilder is more efficient");
     }
+}
+
+public static void main (String[] args){
+
+    int[] random_number_array = random_num(10);
+    avg_timefor_fact(random_number_array);
+    time_takenby_Stringto_concatenate(10);
+  
 }
 
 
